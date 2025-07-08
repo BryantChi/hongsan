87d726e7febf2217324f3da8fb95c96e6cfa5ab4
BryantChi <bryantchi.work@gmail.com>
2025-07-09 02:22:24 +0800

    [MOD] modify product-intro catalog to save

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Admin\CreateCatalogInfoRequest;
+use App\Models\Admin\CatalogInfo;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
     //
-    public function download(Request $request)
+    public function download(CreateCatalogInfoRequest $request)
     {
         // 檢查honeypot欄位
         if (!empty($request->input('website'))) {
@@ -17,25 +19,31 @@ class CatalogController extends Controller
             ], 403);
         }
 
-        // 驗證表單數據
-        $validated = $request->validate([
-            'name' => 'required',
-            'phone' => 'required',
-            'location' => 'required',
-            'product_id' => 'required',
-            'product_pdf' => 'required',
-        ]);
+        // product_pdf欄位必須存在
+        if (!$request->has('product_pdf') || empty($request->input('product_pdf'))) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product PDF is Not Found'
+            ], 422);
+        }
 
+        // 驗證表單數據
         // 儲存聯絡資訊到資料庫 (如有需要)
-        // ...
+        $input = $request->all();
 
         // 取得PDF檔案路徑
-        $pdfPath = public_path('uploads/' . $request->product_pdf);
+        $pdfPathOriginal = $input['product_pdf'];
+        // $pdfPath = public_path('uploads/' . $pdfPathOriginal);
+
+        // 不包含 honeypot、product_pdf欄位
+        unset($input['website'], $input['product_pdf']);
+
+        CatalogInfo::create($input);
 
         // 回傳成功訊息和下載URL
         return response()->json([
             'success' => true,
-            'download_url' => asset('uploads/' . $request->product_pdf)
+            'download_url' => asset('uploads/' . $pdfPathOriginal)
         ]);
     }
 }
